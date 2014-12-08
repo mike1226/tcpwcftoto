faf11a4a5bc210c809e26f65647b22060772a8c7
mike1226 <haifengya@gmail.com>
2014-12-08 17:44:45 +0800

    重新定义的基类

---
+++ Test Client/Games/frmGameList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using  Test_Client;
+
+namespace Test_Client.Games
+{
+    /// <summary>
+    /// 比赛列表
+    /// </summary>
+    public partial class frmGameList :frmBase
+    {
+        public frmGameList()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 从DB中取得数据
+        /// </summary>
+        private void BindGames()
+        {
+            
+        }
+
+    }
+}
